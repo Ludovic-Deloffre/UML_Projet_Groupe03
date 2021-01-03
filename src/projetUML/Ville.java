607a63dfb57ec8488548797bd50efc8e3701f6d0
Alexandre <alexandre.jallade@alumni.univ-avignon.fr>
2021-01-03 16:41:31 +0100

    add list

--- src/projetUML/Ville.java
+++ src/projetUML/Ville.java
@@ -1,17 +1,18 @@
 package projetUML;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Ville {
-    ArrayList<ArrayList<Case> > carte = new ArrayList<ArrayList<Case>>();
+    List<ArrayList<Case> > carte = new ArrayList<ArrayList<Case>>();
 
     public Ville (int taille1,int taille2){
         for(int i=0 ; i<=taille1 ; i++){
-            ArrayList<Case> a1 = new ArrayList<Case>(taille2);
+            List<Case> a1 = new ArrayList<Case>(taille2);
             for(int u=0 ; u<=taille2 ; u++){
                 a1.add(null);
             }
-            carte.add(a1);
+            carte.add((ArrayList<Case>) a1);
         }
     }
 }
